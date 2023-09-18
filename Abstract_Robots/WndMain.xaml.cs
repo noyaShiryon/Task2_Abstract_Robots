a79f160d77a8e81977923e568e87bff03428f769
noyaShiryon <noya.shiryon@gmail.com>
2023-09-18 15:25:37 +0300

    Update WndMain.xaml.cs

--- Abstract_Robots/WndMain.xaml.cs
+++ Abstract_Robots/WndMain.xaml.cs
@@ -30,8 +30,8 @@ namespace Robots_inc
             activeWorkers = lw;
             activeMissions = lm;
             activeRobots = lr;
-            this.Content = null;
-            this.Content = worker = w;
+            this.DataContext = null;
+            this.DataContext = worker = w;
         }
 
         private void LoadWorkerInfo()
